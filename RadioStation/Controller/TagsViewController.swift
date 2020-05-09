969840c8c466731c6f6a1c02b5e9717e8a3654b3
Airam Rguez <airamrguezrivero@gmail.com>
2020-05-09 13:20:16 +0200

    setup

---
+++ RadioStation/Controller/TagsViewController.swift
@@ -0,0 +1,28 @@
+//
+//  ViewController.swift
+//  RadioStation
+//
+//  Created by Airam Rguez on 09/05/2020.
+//  Copyright © 2020 Airam Rguez. All rights reserved.
+//
+
+import UIKit
+
+class TagsViewController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+}
+
+// MARK: - Table view delegate
+extension TagsViewController {
+    
+}
+
+// MARK: - Table view data source
+extension TagsViewController {
+    
+}
